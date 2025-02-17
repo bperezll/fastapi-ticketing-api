7e918a31ec4db225e0553e59dc28c5f7675617c1
bperezll <159130132+bperezll@users.noreply.github.com>
2025-02-17 09:31:16 +0000

    Update client.py
    Changes on client

--- db/client.py
+++ db/client.py
@@ -1,8 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()  # Carga variables del archivo .env
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_SERVER = os.getenv("DB_SERVER")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 # Create an Engine
-mariadb_url = f"mariadb+pymysql://{"username"}:{"password"}@{"server"}:{"port"}/{"database_name"}"
+mariadb_url = f"mariadb+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(mariadb_url, echo=True)
 
 # Create session to interact with database
